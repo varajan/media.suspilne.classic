0f760a204fd4f91a796c67e17a044762fb5070b5
Sergii Dmytrenko <SEDO@simcorp.com>
2021-02-23 17:07:30 +0200

    try to catch outOfMemory exception

--- app/src/main/java/media/suspilne/classic/ImageHelper.java
+++ app/src/main/java/media/suspilne/classic/ImageHelper.java
@@ -38,25 +38,29 @@ public class ImageHelper {
     }
 
     public static Bitmap getCircularDrawable(Bitmap bitmap) {
-        if (bitmap == null || bitmap.isRecycled()) {
-            return null;
+        try{
+            if (bitmap == null || bitmap.isRecycled()) {
+                return null;
+            }
+            float radius = bitmap.getWidth() > bitmap.getHeight() ? ((float) bitmap
+                    .getHeight()) / 2f : ((float) bitmap.getWidth()) / 2f;
+            Bitmap canvasBitmap = Bitmap.createBitmap(bitmap.getWidth(),
+                    bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+            BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP,
+                    Shader.TileMode.CLAMP);
+            Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setShader(shader);
+
+            Canvas canvas = new Canvas(canvasBitmap);
+
+            canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+                    radius, paint);
+
+            return canvasBitmap;
+        }catch (Exception ex){
+            return bitmap;
         }
-        float radius = bitmap.getWidth() > bitmap.getHeight() ? ((float) bitmap
-                .getHeight()) / 2f : ((float) bitmap.getWidth()) / 2f;
-        Bitmap canvasBitmap = Bitmap.createBitmap(bitmap.getWidth(),
-                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
-        BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP,
-                Shader.TileMode.CLAMP);
-        Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setShader(shader);
-
-        Canvas canvas = new Canvas(canvasBitmap);
-
-        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
-                radius, paint);
-
-        return canvasBitmap;
     }
 
     public static Bitmap getBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
